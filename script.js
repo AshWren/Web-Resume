7b4df3c4404bd6e5f3e6a908677af64c8d101a6a
Rachel Stuckey <rachelstuckey@yahoo.com>
2017-01-18 13:06:53 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+
+    hideAll();
+
+    function hideAll() {
+        $('#introduction').hide();
+        console.log("hello");
+        $('#education').hide();
+        $('#employment').hide();
+        $('#skills').hide();
+        $('#portfolio').hide();
+        $('#contact').hide();
+
+        $('#introBtn').removeClass('active');
+        $('#eduBtn').removeClass('active');
+        $('#empBtn').removeClass('active');
+        $('#skillsBtn').removeClass('active');
+        $('#portBtn').removeClass('active');
+        $('#contBtn').removeClass('active');
+    };
+
+    function showSection(idClicked) {
+        switch (idClicked) {
+            case "introBtn":
+                $('#introduction').fadeIn();
+                $('#introBtn').addClass("active");
+                break;
+            case "eduBtn":
+                $('#education').fadeIn();
+                $('#eduBtn').addClass("active");
+                break;
+        };
+    };
+
+    function hideSection(idClicked) {
+        switch (idClicked) {
+            case "introBtn":
+                $('#introduction').fadeOut();
+                $('#introBtn').removeClass("active");
+                break;
+            case "eduBtn":
+                $('#education').fadeOut();
+                $('#eduBtn').removeClass("active");
+                break;
+        };
+    };
+
+    $('#introBtn').on('click', function () {
+        if ($('#introBtn').hasClass("active")) {
+            hideSection('introBtn');
+        } else {
+            hideAll();
+            showSection('introBtn');
+        }
+    });
+
+    $('#eduBtn').on('click', function () {
+        if ($('#eduBtn').hasClass("active")) {
+            hideSection('eduBtn');
+        } else {
+            hideAll();
+            showSection('eduBtn');
+        }
+    });
+
+    $('#empBtn').on('click', function () {
+        hideAll();
+        toggleSection('empBtn');
+    });
+    $('#skillsBtn').on('click', function () {
+        hideAll();
+        toggleSection('skillsBtn');
+    });
+    $('#portBtn').on('click', function () {
+        hideAll();
+        toggleSection('portBtn');
+    });
+    $('#contBtn').on('click', function () {
+        hideAll();
+        toggleSection('contBtn');
+    });
+
+});
